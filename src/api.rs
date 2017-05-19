951a8b3bf77a4751be3d9c1c3bd8cd712bd3762b
Kamil Markiewicz <k.a.markiewicz@gmail.com>
2017-05-19 15:19:46 +0200

    deserialize all possible API calls

--- src/api.rs
+++ src/api.rs
@@ -2,7 +2,7 @@ use errors::*;
 use serde_json;
 
 #[derive(Debug, Deserialize, PartialEq)]
-#[serde(rename_all = "lowercase")]
+#[serde(rename_all = "snake_case")]
 pub enum Api {
     Open {
         baza: String,
@@ -14,6 +14,125 @@ pub enum Api {
         secret: String,
         newlogin: String,
         newpassword: String,
+    },
+
+    Event {
+        login: String,
+        password: String,
+        eventname: String,
+        start_timestamp: String,
+        end_timestamp: String,
+    },
+
+    User {
+        login: String,
+        password: String,
+        newlogin: String,
+        newpassword: String,
+    },
+
+    Talk {
+        login: String,
+        password: String,
+        speakerlogin: String,
+        talk: String,
+        title: String,
+        start_timestamp: String,
+        room: String,
+        initial_evaluation: String,
+        eventname: String,
+    },
+
+    RegisterUserForEvent {
+        login: String,
+        password: String,
+        eventname: String,
+    },
+
+    Attendance {
+        login: String,
+        password: String,
+        talk: String,
+    },
+
+    Evaluation {
+        login: String,
+        password: String,
+        talk: String,
+        rating: String,
+    },
+
+    Reject {
+        login: String,
+        password: String,
+        talk: String,
+    },
+
+    Proposal {
+        login: String,
+        password: String,
+        talk: String,
+        title: String,
+        start_timestamp: String,
+    },
+
+    Friends {
+        login1: String,
+        password: String,
+        login2: String,
+    },
+
+    UserPlan { login: String, limit: String },
+
+    DayPlan { timestamp: String },
+
+    BestTalks {
+        start_timestamp: String,
+        end_timestamp: String,
+        limit: String,
+        all: String,
+    },
+
+    MostPopularTalks {
+        start_timestamp: String,
+        end_timestamp: String,
+        limit: String,
+    },
+
+    AttendedTalks { login: String, password: String },
+
+    AbandonedTalks {
+        login: String,
+        password: String,
+        limit: String,
+    },
+
+    RecentlyAddedTalks { limit: String },
+
+    RejectedTalks { login: String, password: String },
+
+    Proposals { login: String, password: String },
+
+    FriendsTalks {
+        login: String,
+        password: String,
+        start_timestamp: String,
+        end_timestamp: String,
+        limit: String,
+    },
+
+    FriendsEvents {
+        login: String,
+        password: String,
+        event: String,
+    },
+
+    RecommendedTalks {
+        login: String,
+        password: String,
+        start_timestamp: String,
+        end_timestamp: String,
+        limit: String,
     },
 }
 
@@ -62,6 +181,20 @@ mod tests {
                     secret: SECRET.to_owned(),
                     newlogin: "organizer".to_owned(),
                     newpassword: "d8578edf8458ce06fbc".to_owned(),
+                });
+    }
+
+    #[test]
+    fn deserialize_snake_case() {
+        let data =
+            r#"{ "most_popular_talks": {
+            "start_timestamp": "2017-06-05", "end_timestamp": "2017-06-14", "limit": "42"}}"#;
+        let info: Api = read_call(&data).unwrap();
+        assert!(info ==
+                Api::MostPopularTalks {
+                    start_timestamp: "2017-06-05".to_owned(),
+                    end_timestamp: "2017-06-14".to_owned(),
+                    limit: "42".to_owned(),
                 });
     }
 
