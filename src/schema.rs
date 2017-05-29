a9f630e08766f1d24b32eb3f750a961c64bd1587
Kamil Markiewicz <k.a.markiewicz@gmail.com>
2017-05-29 16:37:05 +0200

    diesel print-schema

--- src/schema.rs
+++ src/schema.rs
@@ -1,9 +1,52 @@
-
+table! {
+    Event (event_id) {
+        event_id -> Int8,
+        eventname -> Text,
+        start_timestamp -> Timestamp,
+        end_timestamp -> Timestamp,
+    }
+}
 
 table! {
-    User {
-        id -> Integer,
-        name -> VarChar,
+    Talk (talk_id) {
+        talk_id -> Text,
+        status -> Int2,
+        title -> Text,
+        user_id_User -> Nullable<Int8>,
+        event_id_Event -> Nullable<Int8>,
+        start_timestamp -> Timestamp,
+        add_timestamp -> Timestamp,
+    }
+}
+
+table! {
+    User (user_id) {
+        user_id -> Int8,
+        login -> Varchar,
+        password -> Text,
         is_organizer -> Bool,
+    }
+}
+
+table! {
+    User_attended_for_Talk (user_id_User, talk_id_Talk) {
+        user_id_User -> Int8,
+        talk_id_Talk -> Text,
+        present -> Bool,
+        rating -> Nullable<Int2>,
+    }
+}
+
+table! {
+    User_friend_of_User (user1_id, user2_id) {
+        user1_id -> Int8,
+        user2_id -> Int8,
+    }
+}
+
+table! {
+    User_registered_for_Event (user_id_User, event_id_Event) {
+        user_id_User -> Int8,
+        event_id_Event -> Int8,
     }
 }
