7d539549e1cbde6cf4275aced49250148a6c532a
Kamil Markiewicz <k.a.markiewicz@gmail.com>
2017-06-11 11:03:16 +0200

    cargo fmt

--- src/database.rs
+++ src/database.rs
@@ -170,11 +170,7 @@ pub fn register_user_for_event(conn: &Connection,
 
 /// (*U) attendance <login> <password> <talk>
 /// odnotowanie faktycznej obecności uczestnika <login> na referacie <talk>
-pub fn attendance(conn: &Connection,
-                  login: String,
-                  password: String,
-                  talk: String)
-                  -> Result<()> {
+pub fn attendance(conn: &Connection, login: String, password: String, talk: String) -> Result<()> {
     let person_id = authorize_person_as(conn, login, Some(password), PersonType::Participant)?;
 
     let talk_id: i32 = conn.query(r#"
